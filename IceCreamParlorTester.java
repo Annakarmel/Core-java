1c52a9ab0653cdd96623decf49ac7dc54925cffe
Annakarmel <114638155+Annakarmel@users.noreply.github.com>
2022-11-02 02:09:08 +0530

    Add files via upload

---
+++ IceCreamParlorTester.java
@@ -0,0 +1,34 @@
+package com.xworkz.medicalshopapp;
+
+public class IceCreamParlorTester {
+
+	public static void main(String[] args) {
+		
+		/*String brand[] = new String[3];
+		brand[0] = "Arun";
+		brand[1] = "Amul";
+		brand[2] = "Hangos";
+		
+		String type[] = new String[3];
+		type[0] = "Bar";
+		type[1] = "Cup";
+		type[2] = "Cone";
+		
+		String flavor[] = new String[3];
+		flavor[0] = "Mango";
+		flavor[1] = "ButterScotch";
+		flavor[2] = "venila";
+		
+		String billingMethod[] = new String[3];
+		billingMethod[0] = "Cash";
+		billingMethod[1] = "Scanner";
+		billingMethod[2] = "Debit";
+		
+		IceCreamParlor ice = new IceCreamParlor(brand,type,flavor, billingMethod);*/
+		
+		IceCreamParlor ice = new IceCreamParlor();
+		ice.icecreamInfo();
+		
+	}
+
+}
