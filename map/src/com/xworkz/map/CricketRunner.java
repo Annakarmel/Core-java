910c25312f304cb15b5f3c0592e4fbe08c930573
Annakarmel22 <annakarmel.xworkz@gmail.com>
2023-01-16 02:48:41 +0530

    committing to files

---
+++ map/src/com/xworkz/map/CricketRunner.java
@@ -0,0 +1,24 @@
+package com.xworkz.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CricketRunner {
+
+	public static void main(String[] args) {
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("Dhoni", 10000);
+		map.put("Virat", 300000);
+		map.put("Sachin", 70000);
+		map.put("Pandya", 56000);
+		map.put("ABD", 2003);
+		
+		Set<java.util.Map.Entry<String, Integer>> entries = map.entrySet();
+		entries.stream().filter(e->e.getValue()<10000).forEach(e->System.out.println(e.getKey()+" "+ e.getValue()));
+		
+
+	}
+
+}
